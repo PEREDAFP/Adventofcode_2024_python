4b6e5c34d0507ec9d56501f7b329b79e6b420194
pedro <pedropereda@hotmail.com>
2025-08-22 18:59:55 +0200

    Modificaciones en el init.py de dias

--- dias/__init_.py
+++ dias/__init_.py
@@ -1,6 +1,7 @@
 '''
 Cada uno de los módulos de este bloque tendrán como nombre
         diaX
-Tendrán una función resolver( parte: str) 
+Tendrán una función resolver( parte: int, iteraodr) 
 que imprimirá el código del resultado obtenido para poder pasarlo a AdventofCode
+El iterador habrá sído creado previamente en la función main invocadora
 '''
