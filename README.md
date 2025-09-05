3b1290265f675797942f207e7ed51c8488e1fdfd
pedro <pedropereda@hotmail.com>
2025-09-05 19:34:06 +0200

    Solución día6 parte 2

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Este repositorio contiene mis respuestas y soluciones a los retos de **[Advent of Code 2024](https://adventofcode.com/2024)**.  
 El objetivo no es competir ni optimizar al máximo, sino simplemente divertirme, practicar y disfrutar resolviendo los puzzles diarios.
-El código está realizado en pythons
+El código está realizado en python.
 
 ## Estructura del directorio dias
 
@@ -16,6 +16,6 @@ python main.py --file fichero --dia 1 --parte 1
 
 ## Nota
 
-- El código puede no ser la solución más eficiente.
+- El código puede no ser la solución más eficiente. El reto consiste en utilizar programación funcional y comprehensive list lo más posible.
 - Se trata únicamente de un **ejercicio personal y de entretenimiento**.
 - Siéntete libre de curiosear, comentar o comparar con tus propias soluciones.
--- dias/dia6.py
+++ dias/dia6.py
@@ -1,26 +1,30 @@
+
+OBSTACULO = '#'
+
+# Direcciones: arriba, derecha, abajo, izquierda    
+DIRECCIONES = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+SIMBOLOS = {'^': 0, '>': 1, 'v': 2, '<': 3}
+
 def contar_pasos_1(mapa):
-    OBSTACULO = '#'
+    
     filas = len(mapa)
     columnas = len(mapa[0])
     visitadas = []
    
-    # Direcciones: arriba, derecha, abajo, izquierda
-    direcciones = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-    simbolos = {'^': 0, '>': 1, 'v': 2, '<': 3}
 
     # Encontrar posición inicial y dirección
     x, y, dir_idx = next(
-        ( (i, j, simbolos[mapa[i][j]]) 
+        ( (i, j, SIMBOLOS[mapa[i][j]]) 
         for i in range(filas) 
         for j in range(columnas) 
-        if mapa[i][j] in simbolos ),
+        if mapa[i][j] in SIMBOLOS ),
         (None, None, None)  # valor por defecto si no encuentra nada
     )
     pasos = 0
 
     while True:
         #Incrementamos x e y con la dirección obtenidoa
-        dx, dy = direcciones[dir_idx]
+        dx, dy = DIRECCIONES[dir_idx]
         nx, ny = x + dx, y + dy
 
         # Caso de salida del mapa
@@ -36,6 +40,82 @@ def contar_pasos_1(mapa):
                 pasos += 1
                 visitadas.append((x,y))
            
+def comprobando_bucle(mapa):
+    #Voy creando el recorrido.
+    #Si me encuentro al final del mapa devolveré False porque no hay bucle
+    #Si me encuentro dos veces en la misma posición y dirección será un bucle y saldré con True
+    # Voy a utilizar un set para visitadas
+    filas = len(mapa)
+    columnas = len(mapa[0])
+    visitadas = set()
+   
+    
+    # Encontrar posición inicial y dirección
+    x, y, dir_idx = next(
+        ( (i, j, SIMBOLOS[mapa[i][j]]) 
+        for i in range(filas) 
+        for j in range(columnas) 
+        if mapa[i][j] in SIMBOLOS ),
+        (None, None, None)  # valor por defecto si no encuentra nada
+    )
+    if x == None: return False #Hemos pasado un mapa sin inicio, problablemente al sobreescribir esa casilla con un obstáculo 
+    #Añadimos en visitada la primera posición
+    visitadas.add((x,y, dir_idx))
+    while True:
+        #Incrementamos x e y con la dirección obtenidoa
+        dx, dy = DIRECCIONES[dir_idx]
+        nx, ny = x + dx, y + dy
+
+        # Caso de salida del mapa
+        if not (0 <= nx < filas and 0 <= ny < columnas):
+            return False # No hemos encontrado ningún bucle ytermina
+           
+        # Caso obstáculo
+        if mapa[nx][ny] == OBSTACULO:
+            dir_idx = (dir_idx + 1) % 4  # girar derecha         
+        else:
+            x, y = nx, ny
+            #Creamos un array con la posición x e y, la dirección que tiene en ese momento y el índice de dicha posición
+            #Lo del índice hay que mirarlo, pero creo que facilitará los cálculos en el siguiente paso
+            if (x,y, dir_idx) in visitadas:
+                return True #Hemos encontrado un bucle ya que hemos vuelto a la misma posición con la misma dirección
+            #Añadimos la casilla y la dirección a visitadas       
+            visitadas.add((x,y,dir_idx))
+
+def contamos_bucles(mapa):
+    #Vamos a ir añadiendo obstáculos nuevos al mapa
+    #Si se produce un bucle lo contamos
+    bucles = 0
+    # Direcciones: arriba, derecha, abajo, izquierda
+    filas = len(mapa)
+    columnas = len(mapa[0])
+
+    for y in range(filas):
+        for x in range(columnas):
+            #Vamos a añadir un nuevo obstáculo
+            # Como estamos trabajando con cadenas de caracteres lo hacemos con la función reemplazar
+            #Creamos una copia de mapa para mantener las posiciones que no forman parte de 
+            mapa_cop = mapa.copy()
+            mapa_cop[y] = reemplazar(mapa_cop[y], x, OBSTACULO)
+            bucles += int(comprobando_bucle(mapa_cop))
+
+    return bucles 
+
+
+
+def reemplazar(cadena, pos, char):
+    """
+    Devuelve una nueva cadena con el carácter en la posición `pos` reemplazado por `char`.
+    """
+    if pos < 0 or pos >= len(cadena):
+        raise IndexError("Posición fuera de rango")
+    if len(char) != 1:
+        raise ValueError("Solo se puede reemplazar por un único carácter")
+
+    return cadena[:pos] + char + cadena[pos+1:]
+
+
+
 
 
 def resolver( parte: int, iterador ):
@@ -46,8 +126,9 @@ def resolver( parte: int, iterador ):
     if parte == 1:     
         print(f"El resultado de día 6 parte 1 es {contar_pasos_1(mapa)}")  
   
-    elif parte == 2:       
-        print(f"El resultado de día 5 parte 2 es ")
+    elif parte == 2: 
+        print(f"El resultado de día 5 parte 2 es {contamos_bucles(mapa)} ")
+        
     else:
         print("No existe esa parte")
 
